0dc68cb6f177c323663569bcd7d63a15542de247
nisse23 <58069455+nisse23@users.noreply.github.com>
2021-03-11 16:53:08 +0100

    added the projects

---
+++ Chess/Chess/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Program
+    {
+        static board board = new board();
+
+        static void Main(string[] args)
+        {
+            cell c = new cell(3, 5);
+            board.Markallowedmove(c, "Drottning");
+            //board.placePiecesInStart();
+            PrintBoard(board);
+            Console.ReadLine();
+
+        }
+
+        private static void PrintBoard(board board)
+        {
+            Console.WriteLine("  0  1  2  3  4  5  6  7");
+            for (int i = 0; i < board.size; i++)
+            {
+                Console.Write(i +1);
+                for (int j = 0; j < board.size; j++)
+                {
+                    //Console.Write(j);
+                    cell c = board.TheGrid[i, j];
+                    if(c.Occupied)
+                    {
+                        Console.Write(" X ");
+
+                    }
+                    else if (c.AllowedMove)
+                    {
+                        Console.Write(" + ");
+                    }
+
+                    else
+                    {
+                        Console.Write(" . ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    public class board
+    {
+        //storleken på spelplanen
+        public int size { get; set; }
+        //en 2d array som motsvarar spelplanen
+        public cell[,] TheGrid { get; set; }
+        public board()
+        {
+            size = 8;
+            TheGrid = new cell[size, size];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    TheGrid[i,j] = new cell(i,j);
+
+                }
+            }
+        }
+        //public void placePiecesInStart()
+        //{
+        //    for (int i = 2; i < size; i++)
+        //    {
+        //        for (int j = 7; j > 0; j--)
+        //        {
+        //            //TheGrid[i, j].Occupied = true;
+        //        }
+        //    }
+        //}
+        public void Markallowedmove (cell currentCell, string piece)
+        {
+            TheGrid[currentCell.Columnnumber, currentCell.Rownumber].Occupied = true;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    TheGrid[i, j].AllowedMove = false;
+                }
+            }
+            switch (piece)
+            {
+                case "Torn":
+                    //kan gå vertikalt och horisontellt
+                    for (int i = 0; i < size; i++)
+                    {
+                        if(currentCell.Columnnumber + i < size)
+                        TheGrid[currentCell.Columnnumber + i, currentCell.Rownumber ].AllowedMove = true;
+                        if(currentCell.Rownumber + i < size)
+                        TheGrid[currentCell.Columnnumber , currentCell.Rownumber + i].AllowedMove = true;
+                        if (currentCell.Columnnumber - i >= 0)
+                            TheGrid[currentCell.Columnnumber - i, currentCell.Rownumber].AllowedMove = true;
+                        if (currentCell.Rownumber - i >= 0)
+                            TheGrid[currentCell.Columnnumber, currentCell.Rownumber - i].AllowedMove = true;
+                    }
+
+                    break;
+                case "Kung":
+                    // kan gå ett steg åt alla håll
+                    TheGrid[currentCell.Columnnumber + 1, currentCell.Rownumber].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber - 1, currentCell.Rownumber].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber + 1, currentCell.Rownumber + 1].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber - 1, currentCell.Rownumber + 1].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber + 1, currentCell.Rownumber - 1].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber - 1, currentCell.Rownumber - 1].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber , currentCell.Rownumber + 1].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber , currentCell.Rownumber - 1].AllowedMove = true;
+
+                    break;
+                case "Drottning":
+                    //diagonal / löpare
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (currentCell.Columnnumber + i < size && currentCell.Rownumber + i < size)
+                        {
+                            TheGrid[currentCell.Columnnumber + i, currentCell.Rownumber + i].AllowedMove = true;
+                        }
+                        if (currentCell.Columnnumber + i < size && currentCell.Rownumber - i >= 0)
+                        {
+                            TheGrid[currentCell.Columnnumber + i, currentCell.Rownumber - i].AllowedMove = true;
+                        }
+                        if (currentCell.Columnnumber - i >= 0 && currentCell.Rownumber + i < size)
+                        {
+                            TheGrid[currentCell.Columnnumber - i, currentCell.Rownumber + i].AllowedMove = true;
+                        }
+                        if (currentCell.Columnnumber - i >= 0 && currentCell.Rownumber - i >= 0)
+                        {
+                            TheGrid[currentCell.Columnnumber - i, currentCell.Rownumber - i].AllowedMove = true;
+                        }
+
+                    }
+                    //vertikal o horisontelt / torn
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (currentCell.Columnnumber + i < size)
+                            TheGrid[currentCell.Columnnumber + i, currentCell.Rownumber].AllowedMove = true;
+                        if (currentCell.Rownumber + i < size)
+                            TheGrid[currentCell.Columnnumber, currentCell.Rownumber + i].AllowedMove = true;
+                        if (currentCell.Columnnumber - i >= 0)
+                            TheGrid[currentCell.Columnnumber - i, currentCell.Rownumber].AllowedMove = true;
+                        if (currentCell.Rownumber - i >= 0)
+                            TheGrid[currentCell.Columnnumber, currentCell.Rownumber - i].AllowedMove = true;
+                    }
+                    break;
+                case "Löpare":
+                    //löpare kan gå diagonalt
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (currentCell.Columnnumber + i < size && currentCell.Rownumber + i < size)
+                        {
+                            TheGrid[currentCell.Columnnumber + i, currentCell.Rownumber + i].AllowedMove = true;
+                        }
+                        if (currentCell.Columnnumber + i < size && currentCell.Rownumber - i >= 0)
+                        {
+                            TheGrid[currentCell.Columnnumber + i, currentCell.Rownumber - i].AllowedMove = true;
+                        }
+                        if (currentCell.Columnnumber - i >= 0 && currentCell.Rownumber + i < size)
+                        {
+                            TheGrid[currentCell.Columnnumber - i, currentCell.Rownumber + i].AllowedMove = true;
+                        }
+                        if (currentCell.Columnnumber - i >= 0 && currentCell.Rownumber - i >= 0)
+                        {
+                            TheGrid[currentCell.Columnnumber - i, currentCell.Rownumber - i].AllowedMove = true;
+                        }
+                    }
+                    break;
+                case "Häst":
+                    //hästen kan gå två steg fram och ett åt sidan
+                    TheGrid[currentCell.Columnnumber + 2, currentCell.Rownumber + 1].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber - 2, currentCell.Rownumber + 1].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber + 2, currentCell.Rownumber - 1].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber - 2, currentCell.Rownumber - 1].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber + 1, currentCell.Rownumber + 2].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber - 1, currentCell.Rownumber + 2].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber + 1, currentCell.Rownumber - 2].AllowedMove = true;
+                    TheGrid[currentCell.Columnnumber - 1, currentCell.Rownumber - 2].AllowedMove = true;
+                    break;
+                case "Bonde":
+                    //bonde kan gå ett elr två steg fram
+                    Console.WriteLine("tjo");
+                    TheGrid[currentCell.Columnnumber - 1, currentCell.Rownumber].AllowedMove = true;
+                    Console.WriteLine(currentCell.Columnnumber - 2+" " + currentCell.Rownumber);
+                    TheGrid[currentCell.Columnnumber - 2, currentCell.Rownumber].AllowedMove = true;
+                    Console.WriteLine(TheGrid[currentCell.Columnnumber - 2, currentCell.Rownumber].AllowedMove);
+                    //den kan oxå ta snett fram om det står någon där
+                    if(TheGrid[currentCell.Rownumber + 1,currentCell.Columnnumber +1].Occupied)
+                    {
+                        TheGrid[currentCell.Rownumber + 1, currentCell.Columnnumber + 1].AllowedMove = true;
+                    }
+                    if (TheGrid[currentCell.Rownumber + 1, currentCell.Columnnumber - 1].Occupied)
+                    {
+                        TheGrid[currentCell.Rownumber + 1, currentCell.Columnnumber - 1].AllowedMove = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    public class cell
+    {
+        public int Rownumber { get; set; }
+        public int Columnnumber { get; set; }
+        public bool Occupied { get; set; }
+        public bool AllowedMove { get; set; }
+
+        public cell(int y, int x)
+        {
+            Rownumber = x;
+            Columnnumber = y;
+        }
+    }
+}
---
+++ schack/schack/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace schack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class chessPiece
+        {
+            public string position { get; set; }
+            public string coordinates { get; set; }
+            public chessPiece(string _position, string _coordinates)
+            {
+                position = _position;
+                coordinates = _coordinates;
+            }
+           
+        }
+
+        public class Rook : chessPiece
+        {
+            public Rook(string position, string coordinates) : base (position, coordinates)
+            {
+                
+            }
+            public void PossibleMoves()
+            {
+                string[] lista = position.Split(',');
+                int width = int.Parse(lista[0]);
+
+
+                while (width < 544 && width > 194)
+                {
+                    if (width == 544)
+                    {
+                        width -= 50;
+                        string letter = GetCoordinateLetter(width);
+                         
+                    }
+                }
+            }
+        }
+        private void radioButtonWRook2_CheckedChanged(object sender, EventArgs e)
+        {
+            //gets the square
+            Control parent = radioButtonWRook2.Parent;
+            //converts the position
+            //string position = CheckPosition(parent);
+            string position = parent.Location.X + "," + parent.Location.Y;
+            string coordinates = CheckPosition(parent);
+            Rook Wrook1 = new Rook(position, coordinates);
+            Wrook1.PossibleMoves();
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //gets the square
+            Control parent = radioButtonWRook1.Parent;
+            //converts the position
+            string positition = CheckPosition(parent);
+
+        }
+        string CheckPosition(Control piece)
+        {
+            //variables
+            int height = piece.Location.Y;
+            int width = piece.Location.X;
+            string letter;
+            string number;
+            string coordinate;
+            //gets the equivelant letter of the position
+            letter = GetCoordinateLetter(width);
+           
+            //gets the equivelant number of the position
+            number = GetCoordinateNumber(height);
+            
+
+            MessageBox.Show(letter + number);
+
+            //returns the position
+            return coordinate = letter +" " + number;
+
+            
+        }
+        public static string GetCoordinateLetter(int width)
+        {
+            //gets the equivelant letter
+            switch (width)
+            {
+                case 194:
+                    return "a";
+
+                case 244:
+                    return "b";
+
+                case 294:
+                    return "c";
+
+                case 344:
+                    return "d";
+
+                case 394:
+                    return "e";
+
+                case 444:
+                    return "f";
+
+                case 494:
+                    return "g";
+
+                case 544:
+                    return "h";
+
+                default:
+                    MessageBox.Show("FEL");
+                    return "";
+            }
+        }
+
+        public static string GetCoordinateNumber(int height)
+        {
+            //gets the equivelant number
+            switch (height)
+            {
+                case 420:
+                    return "1";
+                   
+                case 370:
+                    return "2";
+                    
+                case 320:
+                    return "3";
+                    
+                case 270:
+                    return "4";
+                    
+                case 220:
+                    return "5";
+                    
+                case 170:
+                    return "6";
+                    
+                case 120:
+                    return "7";
+                    
+                case 70:
+                    return "8";
+                    
+                default:
+                    MessageBox.Show("FEL");
+                    return "";
+            }
+        }
+
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void radioButton1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void paneld3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void paneld1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
